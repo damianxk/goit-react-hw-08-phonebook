c84fc2adb07174e1ee930a3d984cc83c600f7641
Damian Kowalczyk <116799195+damianxk@users.noreply.github.com>
2023-10-03 18:59:55 +0200

    Update Edit.jsx
    input pattern fix

--- src/components/Edit/Edit.jsx
+++ src/components/Edit/Edit.jsx
@@ -71,8 +71,6 @@ export const EditWindow = () => {
               type="text"
               name="name"
               className={css.formInput}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               value={nameToEdit}
               onChange={e => handleNameChange(e)}
               required
@@ -84,8 +82,6 @@ export const EditWindow = () => {
               type="tel"
               name="number"
               className={css.formInput}
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               value={numberToEdit}
               onChange={e => handleNumberChange(e)}
               required
